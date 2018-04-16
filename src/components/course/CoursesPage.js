3105492f93a093ee7e229305ec5c8ed5188248f7
piotrekmich79 <piotrekmich79@gmail.com>
2018-04-16 23:18:56 +0200

    update

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -3,11 +3,12 @@ import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 import * as courseActions from '../../actions/courseActions';
 import CourseList from './CourseList';
+import {browserHistory} from 'react-router';
 
 class CoursesPage extends React.Component {
   constructor(props, context) {
     super(props, context);
-
+  this.redirectToAddCoursePage = this.redirectToAddCoursePage.bind(this);
   }
 
   courseRow(course, index){
@@ -16,11 +17,19 @@ class CoursesPage extends React.Component {
     );
   }
 
+  redirectToAddCoursePage() {
+    browserHistory.push('/course');
+  }
+
   render() {
     const {courses} = this.props;
     return (
       <div>
         <h1>Courses</h1>
+        <input type="submit"
+          value="Add Course"
+          className="btn btn-primary"
+          onClick={this.redirectToAddCoursePage}/>
         <CourseList courses={courses}/>
       </div>
     );
